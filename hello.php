59421aa935a6f5d0c012af1ba2241aafd692fe1f
mayurQED42 <mayur.gajar@qed42.com>
2019-01-20 19:51:00 +0530

    exception

--- hello.php
+++ hello.php
@@ -226,6 +226,27 @@ echo "Today is " . date("Y.m.d") . "<br>";
 echo "Today is " . date("Y-m-d") . "<br>";
 echo "Today is " . date("l");
 echo "The time is " . date("h:i:sa");
+
+//exception
+//create function with an exception
+function checkNum($number) {
+    if($number>1) {
+      throw new Exception("Value must be 1 or below");
+    }
+    return true;
+  }
+  
+  //trigger exception in a "try" block
+  try {
+    checkNum(2);
+    //If the exception is thrown, this text will not be shown
+    echo 'If you see this, the number is 1 or below';
+  }
+  
+  //catch exception
+  catch(Exception $e) {
+    echo 'Message: ' .$e->getMessage();
+  }
 ?>
 </body>
 </html>
